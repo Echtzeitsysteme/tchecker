624e24296a35ab9ab2dcd06933c2c594cf12f4a0
alzeha <Alexander.Lieb@online.de>
2026-01-13 19:16:13 +0100

    Test pypi (#16)
    Added boost as static lib to the workflow that pushes to pypi.

---
+++ ci-scripts/install_boost_1_81_0.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+wget https://sourceforge.net/projects/boost/files/boost/1.81.0/boost_1_81_0.tar.gz
+tar xf boost_1_81_0.tar.gz
+cd boost_1_81_0
+
+./bootstrap.sh
+
+./b2 link=static cxxflags=-fPIC cflags=-fPIC install
